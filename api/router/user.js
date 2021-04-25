8f1e005a305b17fd5879288168ada01b3dc25585
trongvudinh <taolatrong94@gmail.com>
2021-04-25 23:42:50 +0700

    25/4/2021

--- api/router/user.js
+++ api/router/user.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/getuser');
+const User = require('./../models/user');
+
+router.get('/getuser',(req,res,next)=>{
+    User.find({id: '6085960db543e0451cb92740'} )
+    .populate('lst_friend','id username pass')
+    .populate()
+    .then(data => res.status(200).json({ok:data}))
+    .catch(err => res.status(200).json({ok:err}));
+});
+module.exports = router;    
